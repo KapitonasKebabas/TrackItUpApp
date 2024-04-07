a97c3dd591e8bdd9391f348e834d93f173148718
Vysockis <lukasvysockis1031@gmail.com>
2024-04-08 02:56:31 +0300

    Fixed setDate

--- app/src/androidTest/java/com/example/trackitupapp/UserMedicineUpdateTest.kt
+++ app/src/androidTest/java/com/example/trackitupapp/UserMedicineUpdateTest.kt
@@ -2,6 +2,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.PickerActions.setDate
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
