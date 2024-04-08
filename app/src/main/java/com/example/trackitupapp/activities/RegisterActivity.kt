67c5d2f411399bf01d0027939e8a826d1da8511a
Vysockis <lukasvysockis1031@gmail.com>
2024-04-08 23:51:32 +0300

    Create initial Register activity

---
+++ app/src/main/java/com/example/trackitupapp/activities/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trackitupapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trackitupapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
