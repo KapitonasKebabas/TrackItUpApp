43f7c002e5d787a233c4ee6cdaaf6a63351bece7
Vysockis <lukasvysockis1031@gmail.com>
2024-04-20 01:22:58 +0300

    Pipline Test

--- app/src/main/java/com/example/trackitupapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/trackitupapp/activities/LoginActivity.kt
@@ -81,6 +81,7 @@ class LoginActivity : AppCompatActivity() {
                 }
             })
 
+
         calls.callStatuses(
             applicationContext,
             object : StatusesCallback {
