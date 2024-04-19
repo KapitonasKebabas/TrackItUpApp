9222ae93fa428e0d2f7342ade4bfe92ceb83518f
Vysockis <lukasvysockis1031@gmail.com>
2024-04-20 00:19:26 +0300

    Added statuses call to object

--- app/src/main/java/com/example/trackitupapp/apiServices/Callbacks/StatusesCallback.kt
+++ app/src/main/java/com/example/trackitupapp/apiServices/Callbacks/StatusesCallback.kt
@@ -3,6 +3,6 @@ package com.example.trackitupapp.apiServices.Callbacks
 import com.example.trackitupapp.apiServices.responses.StatusResponse
 
 interface StatusesCallback {
-    fun onSuccess(medicineResponse: List<StatusResponse>)
+    fun onSuccess(statuses: List<StatusResponse>)
     fun onFailure(message: String)
 }
