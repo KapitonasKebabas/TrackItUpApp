7cc71f5507ccaac14c7c5b89c0987ddd96fa7a13
Tomas <106265188+TommyN2O@users.noreply.github.com>
2024-04-21 01:35:53 +0300

    save and cancel button in AskForMedicineActivity

--- app/src/main/java/com/example/trackitupapp/activities/AskForMedicineActivity.kt
+++ app/src/main/java/com/example/trackitupapp/activities/AskForMedicineActivity.kt
@@ -1,6 +1,7 @@
 package com.example.trackitupapp.activities
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trackitupapp.R
 import com.example.trackitupapp.apiServices.ApiCalls
@@ -13,6 +14,22 @@ class AskForMedicineActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ask_for_medicine)
 
         calls = ApiCalls()
+        val inflater = layoutInflater
+        val dialogView = inflater.inflate(R.layout.activity_ask_for_medicine, null)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Ask For Medicine")
+            .setView(dialogView)
+            .setPositiveButton("Save") { _, _ ->
+//                medicine.qty = amountEditText.text.toString().toInt()
+//                medicine.exp_date = expirationEditText.text.toString()
+//                medicine.is_shared = editSwitch.isChecked
+//                updateMedicine(medicine)
+
+            }
+            .setNegativeButton("Cancel") { _, _ ->
+                finish()
+            }
+            .create()
 
 
     }
