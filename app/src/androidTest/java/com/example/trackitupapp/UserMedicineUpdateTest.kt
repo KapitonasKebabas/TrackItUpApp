6e78f86fabd624d4986cbf0099bc640704cf5107
Vysockis <my@email.com>
2024-11-12 01:18:48 +0200

    Fix

--- app/src/androidTest/java/com/example/trackitupapp/UserMedicineUpdateTest.kt
+++ app/src/androidTest/java/com/example/trackitupapp/UserMedicineUpdateTest.kt
@@ -22,5 +22,166 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class UserMedicineUpdateTest {
+    object Constants {
+        val updatedAmount = 55
+        val updatedExpiryDate = DATE_FORMAT.format(DATE_FORMAT.parse("2025-06-10"))
+        val updatedAmountNegative = -111
+        val updatedExpiryDateNegative = DATE_FORMAT.format(DATE_FORMAT.parse("2020-04-10"))
 
+    }
+
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(UserMedicineActivity::class.java)
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun login() {
+            val activityScenario = ActivityScenario.launch(LoginActivity::class.java)
+
+            val username = "test"
+            val password = "test"
+
+
+            onView(withId(R.id.username)).perform(typeText(username), closeSoftKeyboard())
+            onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard())
+
+            onView(withId(R.id.btn_login)).perform(click())
+
+            // Ensure the next screen appears
+            onView(withId(R.id.rv_medicineHolder)).check(matches(isDisplayed()))
+
+            activityScenario.close()
+        }
+    }
+
+    @Test
+    fun testItemPickerSetup() {
+        onView(withId(R.id.rv_medicineHolder))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    1,
+                    click()
+                )
+            )
+
+    }
+
+    @Test
+    fun testDatePickerSetup() {
+        onView(withId(R.id.rv_medicineHolder))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    1,
+                    click()
+                )
+            )
+
+
+        onView(withId(R.id.editExpirationDate)).perform(click())
+        onView(withClassName(CoreMatchers.`is`("android.widget.DatePicker"))).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testCancel() {
+        onView(withId(R.id.rv_medicineHolder))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    1,
+                    click()
+                )
+            )
+
+
+        onView(withText("Cancel")).perform(click())
+    }
+
+    @Test
+    fun testUpdate() {
+        onView(withId(R.id.rv_medicineHolder))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    2,
+                    click()
+                )
+            )
+
+
+        onView(withId(R.id.editAmount))
+            .perform(clearText(), typeText(updatedAmount.toString()), closeSoftKeyboard())
+
+        onView(withId(R.id.editExpirationDate)).perform(click())
+        onView(withClassName(CoreMatchers.`is`("android.widget.DatePicker"))).perform(setDate(2025, 6, 10))
+        onView(withText("OK")).perform(click())
+
+        onView(withText("Save")).perform(click())
+
+        onView(withId(R.id.rv_medicineHolder))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    2,
+                    click()
+                )
+            )
+
+        onView(withId(R.id.editAmount)).check(matches(withText(updatedAmount.toString())))
+        onView(withId(R.id.editExpirationDate)).check(matches(withText(updatedExpiryDate)))
+    }
+
+    @Test
+    fun testUpdateWithShare() {
+        onView(withId(R.id.rv_medicineHolder))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    2,
+                    click()
+                )
+            )
+
+
+        onView(withId(R.id.editAmount))
+            .perform(clearText(), typeText(updatedAmount.toString()), closeSoftKeyboard())
+
+        onView(withId(R.id.editExpirationDate)).perform(click())
+        onView(withClassName(CoreMatchers.`is`("android.widget.DatePicker"))).perform(setDate(2025, 6, 10))
+        onView(withText("OK")).perform(click())
+
+        onView(withId(R.id.editSwitch)).perform(click())
+
+        onView(withText("Save")).perform(click())
+
+        onView(withId(R.id.rv_medicineHolder))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    2,
+                    click()
+                )
+            )
+
+        onView(withId(R.id.editAmount)).check(matches(withText(updatedAmount.toString())))
+        onView(withId(R.id.editExpirationDate)).check(matches(withText(updatedExpiryDate)))
+    }
+
+    @Test
+    fun testUpdateNegativeValues() {
+        onView(withId(R.id.rv_medicineHolder))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    1,
+                    click()
+                )
+            )
+
+
+        onView(withId(R.id.editAmount))
+            .perform(clearText(), typeText("0"), closeSoftKeyboard())
+
+        onView(withId(R.id.editExpirationDate)).perform(click())
+        onView(withClassName(CoreMatchers.`is`("android.widget.DatePicker"))).perform(setDate(2020, 4, 10))
+        onView(withText("OK")).perform(click())
+
+        onView(withText("Save")).perform(click())
+
+        onView(withId(R.id.editAmount)).check(matches(hasErrorText("Amount must be at least 1")))
+    }
 }
